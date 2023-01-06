acbcb251a9e36b65f2ee9904d3fff3de0d8c734d
devyonghee <devyonghee@gmail.com>
2023-01-07 01:35:11 +0900

    change mustache

--- security-application/build.gradle.kts
+++ security-application/build.gradle.kts
@@ -8,15 +8,16 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-//    implementation("org.flywaydb:flyway-core:7.15.0")
+
     implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
