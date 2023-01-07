4b649a45ac2462e3e8283cd1fa5321ad85aff031
devyonghee <devyonghee@gmail.com>
2023-01-08 01:57:19 +0900

    add gradle

---
+++ security-jjwt/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.springframework.boot")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.jetbrains.kotlin.plugin.jpa") version ("1.6.21")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.flywaydb:flyway-mysql")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
